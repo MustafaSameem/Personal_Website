b6b1ea0fe1d35762eefd1dd3d6484efbb3634a7f
mustafasameem <mustafasameem2000@gmail.com>
2023-09-02 14:08:07 -0400

    modified margins as %. Styling also changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,15 +6,15 @@ function Header() {
       <header className="header">
         <div className="nav"> {/* Wrap the navigation elements */}
           <ul>
-            <li className="nav-item"><a href="#contact">Contact</a></li>
-            <li className="nav-item"><a href="#blog">Experience</a></li>
             <li className="nav-item"><a href="#about">About</a></li>
+            <li className="nav-item"><a href="#blog">Experience</a></li>
+            <li className="nav-item"><a href="#contact">Contact</a></li>
           </ul>
         </div>
         <div className="intro">
-          <p>Hi, my name is</p>
-          <h1>Mustafa Sameem</h1>
-          <p className="larger-text">I am a software engineering student</p>
+          <p>Hello, my name is</p>
+          <h1>Mustafa Sameem.</h1>
+          <p className="larger-text">I am a software engineering student in Montreal, QC.</p>
         </div>
       </header>
     );
