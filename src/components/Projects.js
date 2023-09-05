a9116eecf799a31bd98cd0b2c0fa5cb4ce699f19
mustafasameem <mustafasameem2000@gmail.com>
2023-09-04 22:35:39 -0400

    added projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,6 +5,11 @@ function Projects() {
     return (
       <div className="projects">
         <h1 id="projects">Projects <span>[3]</span></h1>
+        <h2>Full-Stack Ecommerce Website <span>(Link coming soon)</span></h2>
+        <p>Python, Flask, Jinja2, HTML, CSS, Javascript, Bootstrap, SQLAlchemy, SQLlite3</p>
+
+        <h2>Algorithm Graph Maker<span>(Link coming soon)</span></h2>
+        <p>C++</p>
       </div>
     );
   }
