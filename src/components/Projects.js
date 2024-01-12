a2e450ba891a61a8968372a8ece321463bcab756
Mustafa Sameem <111101673+MustafaSameem@users.noreply.github.com>
2024-01-12 12:13:15 -0500

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,11 +5,15 @@ function Projects() {
     return (
       <div className="projects">
         <h1 id="projects">Projects <span>[3]</span></h1>
-        <h2>Full-Stack Ecommerce Website <span>(Link coming soon)</span></h2>
-        <p>Python, Flask, Jinja2, HTML, CSS, Javascript, Bootstrap, SQLAlchemy, SQLlite3</p>
 
-        <h2>Algorithm Graph Maker<span>(Link coming soon)</span></h2>
-        <p>C++</p>
+        <h2>Chess Multiplayer Game <span>(Link coming soon)</span></h2>
+        <p>C++, SFML</p>
+        
+        <h2>Spotify and Deezer Music Database</h2>
+        <p>MySQL, Neo4j, Python</p>
+
+        <h2>E-commerce website</h2>
+        <p>ReactJS, NodeJS, Python, HTML, CSS, SQLLite3, SQLAlchemy</p>
       </div>
     );
   }
