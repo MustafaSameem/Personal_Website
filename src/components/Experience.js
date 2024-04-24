1def0fa26c9e4132693c7fa5d06837c828ed1c4f
Mustafa Sameem <111101673+MustafaSameem@users.noreply.github.com>
2024-04-24 19:14:32 -0400

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,26 +2,25 @@ import React from 'react';
 import './Experience.css';
 
 function Experience() {
-    return (
-      <div className="experience">
-        <h1 id="experience">Work Experience <span>[2]</span></h1>
-        
-        <h2>Software Engineer CO-OP @<span className ="place"><a href="https://nrc.canada.ca/en/research-development/research-collaboration/research-centres/automotive-surface-transportation-research-centre" 
-            className="orange-link" target="_blank" rel="noreferrer">National Research Council Canada</a></span></h2>
-        <p className="date">January - August 2023</p>
+  return (
+    <div className="experience">
+      <h1 id="experience">Work Experience <span>[2]</span></h1>
+      
+      <h2>Software Engineer CO-OP @<span className ="place"><a href="https://nrc.canada.ca/en/research-development/research-collaboration/research-centres/automotive-surface-transportation-research-centre" 
+          className="orange-link" target="_blank" rel="noreferrer">National Research Council Canada</a></span></h2>
+      <p className="date">January - August 2023</p>
 
-        <ul>
-          <li>Developed a corporate website integrating advanced mathematical algorithms using Pymeshlab to automate STL
-          <br/>mesh operations for heat transfer simulations, resulting in the discovery of alternate optimized models</li>
-          <li>Leveraged Python's multi-threading capabilities to concurrently execute Flask's server, Pyvista for real-time 
-          <br/>mesh visualization, and Gmsh for surface decomposition, achieving 100x speed improvements</li>
-          <li>Employed GitLab for version control, isolated projects using virtual environments, completed tasks in 
-          <br/>Linux via MSYS and Mingw64, and ensured code quality through rigorous testing, enhancing project scalability</li>
-          <li>Conducted literature reviews on mesh optimization, smoothing, remeshing and boolean transformations,
-          <br/>presented weekly findings and actively collaborated on an innovative manufacturing process</li>
-        </ul>
-      </div>
-    );
-  }
-  
-  export default Experience;
+      <ul>
+        <li>Developed a scientific application, enabling 7 researchers to experiment with 3D meshed geometries.</li>
+        <li>Integrated advanced features using open-source APIs and conducted literature review on mesh research papers.</li>
+        <li>Incorporated multi-threading for real-time 3D visualization with PyVista, enhancing user interaction.</li>
+        <li>Achieved a 150x speed improvement in heat transfer simulations using Python, NumPy, C++, Flask, and JavaScript.</li>
+        <li>Implemented an algorithm with PyMesh leading to a 19% volume retention while maintaining structural integrity.</li>
+        <li>Completed tasks in Linux with virtual environments and utilized GitLab for version control.</li>
+        <li>Presented findings weekly to all researchers, engaging in collaborative discussions.</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Experience;
