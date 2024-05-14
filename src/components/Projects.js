37bfec23442c7b71ca7cc5c772ddb936726d3657
Mustafa Sameem <111101673+MustafaSameem@users.noreply.github.com>
2024-05-14 10:52:52 -0400

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,16 +6,13 @@ function Projects() {
       <div className="projects">
         <h1 id="projects">Projects <span>[3]</span></h1>
 
-        <h2>Chess Multiplayer Game <span>(Link coming soon)</span></h2>
+        <h2>Artificial Intelligence Chess Engine @<span>(<a href="https://github.com/MustafaSameem/Chess-Engine" 
+          className="orange-link" target="_blank" rel="noreferrer">Link</a>)</span></h2>
         <p>C++, SFML</p>
-        
-        <h2>Spotify and Deezer Music Database</h2>
-        <p>MySQL, Neo4j, Python</p>
 
-        <h2>E-commerce website</h2>
-        <p>ReactJS, NodeJS, Python, HTML, CSS, SQLLite3, SQLAlchemy</p>
       </div>
     );
-  }
-  
-  export default Projects;
+}
+
+export default Projects;
+
