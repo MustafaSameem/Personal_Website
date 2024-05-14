c816498b1d760702db499bc4d6cca8387ed030ea
Mustafa Sameem <111101673+MustafaSameem@users.noreply.github.com>
2024-05-14 10:57:45 -0400

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,9 +6,20 @@ function Projects() {
       <div className="projects">
         <h1 id="projects">Projects <span>[3]</span></h1>
 
-        <h2>Artificial Intelligence Chess Engine @<span>(<a href="https://github.com/MustafaSameem/Chess-Engine" 
+        <h2>Condo Management System <span>(<a href="https://soen-390-project.onrender.com/" 
+          className="orange-link" target="_blank" rel="noreferrer">Link</a>)</span></h2>
+        <p>NodeJs, React, TypeScript, MongoDB, AWS, Firebase, Docker</p>
+              
+        <h2>Artificial Intelligence Chess Engine <span>(<a href="https://github.com/MustafaSameem/Chess-Engine" 
           className="orange-link" target="_blank" rel="noreferrer">Link</a>)</span></h2>
         <p>C++, SFML</p>
+
+        <h2>Client Server Banking System <span>(<a href="https://github.com/MustafaSameem/Asynchronous-banking-System" 
+          className="orange-link" target="_blank" rel="noreferrer">Link</a>)</span></h2>
+        <p>Java</p>
+
+        <h2>Spotify Music Database</h2>
+        <p>MySQL, Neo4j, Python</p>
 
       </div>
     );
